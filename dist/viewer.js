cb0da51343ba4063f4a3ae25954aefcacade501b
Arthur Shpakov <arthur@amplience.com>
2017-05-26 18:26:42 +0300

    built with reverted spin fix with binding events to document instead of elements

--- dist/viewer.js
+++ dist/viewer.js
@@ -6287,26 +6287,14 @@ amp.stats.event = function(dom,type,event,value){
                     return self._endDrag(e,o,mx,my,i);
                 }
             }(this._index);
-
-            var $innerSpin =  this.element.find('.amp-spin');
-            var $parentSpin = this.element.parents('.amp-spin');
-
-            if($innerSpin.length > 0 || $parentSpin.length > 0){
                 this.$document.on(this.options.events.move, m);
                 this.$document.on(this.options.events.end,u);
-            }
-
-            else {
-                this.element.on(this.options.events.move, m);
-                this.element.on(this.options.events.end, u);
-            }
 
             this._mouseMoveInfo = [{e:e,o:o,mx:mx,my:my,sindex:this._index}];
             if(window.navigator.userAgent.indexOf("MSIE ")>0){
                 return false;
             }
-
-            $innerSpin.each(function(i, element){
+            this.element.find('.amp-spin').each(function(i, element){
                 var childSpin = $(element).data()['amp-ampSpin'];
                 if(childSpin && childSpin._startDrag){
                     childSpin._startDrag(e);
@@ -6386,20 +6374,8 @@ amp.stats.event = function(dom,type,event,value){
             this._ended = true;
 
             this._track("endMove",{'domEvent': e});
-
-            var $innerSpin =  this.element.find('.amp-spin');
-            var $parentSpin = this.element.parents('.amp-spin');
-
-            if($innerSpin.length > 0 || $parentSpin.length > 0) {
                 this.$document.off(this.options.events.end, this._ubind);
                 this.$document.off(this.options.events.move, this._mbind);
-            }
-
-            else{
-                this.element.off(this.options.events.end,this._ubind);
-                this.element.off(this.options.events.move, this._mbind);
-            }
-
             clearInterval(this._timer);
 
             this._setCursor(this.options.cursor.inactive);
@@ -6804,7 +6780,7 @@ Handlebars.registerPartial("nav-container-list-item", this["amp"]["templates"]["
     + "?"
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.templates : depth0)) != null ? stack1.thumb : stack1), depth0))
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.locale : depth0)) != null ? stack1.second : stack1), depth0))
-    + "\"\n        alt=\"\"\n        class=\"amp-main-img thumbnail\">\n    <div class=\"amp-margin-helper\"></div>\n</li>\n";
+    + "\"\n        alt=\"\"\n        class=\"amp-main-img thumbnail\">\n</li>\n";
 },"useData":true}));
 
 Handlebars.registerPartial("nav-container-list", this["amp"]["templates"]["nav-container-list"] = Handlebars.template({"1":function(depth0,helpers,partials,data,blockParams,depths) {
